168b10a2fc3d989d477949dbe5f4196f1b8f9893
Franco Pacheco <pachecozux@gmail.com>
2022-10-28 10:34:56 -0300

    commit inicial, app funcional

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Button, Image, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [image, setImage] = useState(null);
+  //Buscar Imagen
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  // Usar camara
+  const onCameraPress = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      saveToPhotos: true,
+      mediaType: "photo",
+      includeBase64: false,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Seleccionar Imagen" onPress={pickImage} />
+      <Button title="Cámara" onPress={onCameraPress} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
